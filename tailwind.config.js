296ec0cd1fe92367bd289d337de2e62c55947816
ashutosh <ashutosh010sharma@gmail.com>
2024-05-04 11:30:49 +0530

    new branch

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customGreen1: '#527853',
+        customGreen2: '#F9E8D9',
+        customGreen3: '#F7B787',
+        customGreen4: '#EE7214',
+      },
+    },
+  },
+  plugins: [],
+}
+
